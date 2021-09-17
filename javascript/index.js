05f6c149a8edef96cab472978f7b2ce865c455c0
pawansde <pawanchaudhari82@gmail.com>
2021-09-17 13:41:29 +0530

    First commet

---
+++ javascript/index.js
@@ -0,0 +1,33 @@
+
+function f1()
+{
+
+document.getElementById("p1").src="picture/back5.jpg";
+
+}
+
+function f2()
+{
+
+    document.getElementById("p1").src="picture/back2.jpg";
+
+}
+
+
+function f3()
+{
+
+    document.getElementById("p1").src="picture/back3.jpg";
+
+}
+
+
+function f4()
+{
+
+    document.getElementById("p1").src="picture/back4.jpg";
+
+}
+
+
+
